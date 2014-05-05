31a80b83fd84ea2164483920e77154d4dd5a08ac
Mayur Rustagi <mayur@sigmoidanalytics.com>
2014-05-06 01:04:26 +0530

    Fixing load converter to use PigStorage

--- src/org/apache/pig/backend/hadoop/executionengine/spark_streaming/converter/LoadConverter.java
+++ src/org/apache/pig/backend/hadoop/executionengine/spark_streaming/converter/LoadConverter.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.pig.LoadFunc;
+import org.apache.pig.backend.hadoop.executionengine.mapReduceLayer.PigInputFormat;
 import org.apache.pig.backend.hadoop.executionengine.physicalLayer.PhysicalOperator;
 import org.apache.pig.backend.hadoop.executionengine.physicalLayer.plans.PhysicalPlan;
 import org.apache.pig.backend.hadoop.executionengine.physicalLayer.relationalOperators.POLoad;
@@ -18,10 +19,16 @@ import org.apache.pig.impl.PigContext;
 import org.apache.pig.impl.io.FileSpec;
 import org.apache.pig.impl.plan.OperatorKey;
 import org.apache.pig.impl.util.ObjectSerializer;
+import org.apache.spark.streaming.DStream;
 import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.Text;
 
 import scala.Function1;
+import scala.Tuple2;
+import scala.reflect.ClassManifest;
 import scala.runtime.AbstractFunction1;
 
 import com.google.common.collect.Lists;
@@ -36,7 +43,7 @@ import com.google.common.collect.Lists;
 @SuppressWarnings({ "serial"})
 public class LoadConverter implements POConverter<Tuple, Tuple, POLoad> {
 
-	private static final Function1<String, Tuple> TO_VALUE_FUNCTION = new ToTupleFunction();
+	private static final Function1<Tuple2<Text, Tuple>, Tuple> TO_VALUE_FUNCTION = new ToTupleFunction();
      
     private PigContext pigContext;
     private PhysicalPlan physicalPlan;
@@ -53,25 +60,22 @@ public class LoadConverter implements POConverter<Tuple, Tuple, POLoad> {
 //        if (predecessors.size()!=0) {
 //            throw new RuntimeException("Should not have predecessors for Load. Got : "+predecessors);
 //        }
-
-        JobConf loadJobConf = SparkUtil.newJobConf(pigContext);
-        configureLoader(physicalPlan, poLoad, loadJobConf);
-
-        // don't know why but just doing this cast for now
-        JavaDStream<String> hadoopRDD = sparkContext.textFileStream(poLoad.getLFile().getFileName());
-       
+    		configureLoader(physicalPlan, poLoad, sparkContext.ssc().sc().hadoopConfiguration(),this.pigContext);
+        DStream<Tuple2<Text, Tuple>> hadoopRDD= sparkContext.ssc().fileStream(poLoad.getLFile().getFileName(), 
+				SparkUtil.getManifest(Text.class), 
+				SparkUtil.getManifest(Tuple.class), 
+				SparkUtil.getManifest(PigInputFormat.class));
         // map to get just RDD<Tuple>
-        return new JavaDStream<Tuple>(hadoopRDD.dstream().map(TO_VALUE_FUNCTION,SparkUtil.getManifest(Tuple.class)), SparkUtil.getManifest(Tuple.class));
+        return new JavaDStream<Tuple>(hadoopRDD.map(TO_VALUE_FUNCTION,SparkUtil.getManifest(Tuple.class)), 
+        		SparkUtil.getManifest(Tuple.class));
     }
 
-    private static class ToTupleFunction extends AbstractFunction1<String, Tuple>
-            implements Function1<String, Tuple>, Serializable {
+    private static class ToTupleFunction extends AbstractFunction1<Tuple2<Text, Tuple>,Tuple>
+            implements Function1<Tuple2<Text, Tuple>,Tuple>, Serializable {
 
         @Override
-        public Tuple apply(String v1) {
-        	DefaultTuple var=new DefaultTuple();
-			var.append(v1);
-        	return var;
+        public Tuple apply(Tuple2<Text, Tuple> v1) {
+        	return v1._2;
         }
     }
 
@@ -80,14 +84,14 @@ public class LoadConverter implements POConverter<Tuple, Tuple, POLoad> {
      * TODO: refactor it to share this
      * @param physicalPlan
      * @param poLoad
-     * @param jobConf
+     * @param configuration
      * @return
      * @throws java.io.IOException
      */
-    private static JobConf configureLoader(PhysicalPlan physicalPlan,
-                                           POLoad poLoad, JobConf jobConf) throws IOException {
+    private static Configuration configureLoader(PhysicalPlan physicalPlan,
+                                           POLoad poLoad, Configuration configuration, PigContext pigContext) throws IOException {
 
-        Job job = new Job(jobConf);
+        Job job = new Job(configuration);
         LoadFunc loadFunc = poLoad.getLoadFunc();
 
         loadFunc.setLocation(poLoad.getLFile().getFileName(), job);
@@ -113,11 +117,12 @@ public class LoadConverter implements POConverter<Tuple, Tuple, POLoad> {
         inpSignatures.add(poLoad.getSignature());
         inpLimits.add(poLoad.getLimit());
 
-        jobConf.set("pig.inputs", ObjectSerializer.serialize(pigInputs));
-        jobConf.set("pig.inpTargets", ObjectSerializer.serialize(inpTargets));
-        jobConf.set("pig.inpSignatures", ObjectSerializer.serialize(inpSignatures));
-        jobConf.set("pig.inpLimits", ObjectSerializer.serialize(inpLimits));
-
-        return jobConf;
+        configuration.set("pig.inputs", ObjectSerializer.serialize(pigInputs));
+        configuration.set("pig.inpTargets", ObjectSerializer.serialize(inpTargets));
+        configuration.set("pig.inpSignatures", ObjectSerializer.serialize(inpSignatures));
+        configuration.set("pig.inpLimits", ObjectSerializer.serialize(inpLimits));
+        configuration.set("pig.pigContext", ObjectSerializer.serialize(pigContext));
+        configuration.set("udf.import.list", ObjectSerializer.serialize(PigContext.getPackageImportList()));
+        return configuration;
     }
 }
